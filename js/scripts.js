ea911df9a018e5c82234cabf54037eb6f9759dd2
joaogablira <joaogab.lira24@gmail.com>
2024-10-01 22:38:13 -0300

    site semi pronto-2

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,70 @@
 //
 // Scripts
 // 
+const toggleThemeBtn = document.getElementById('toggleTheme');
+const lightIcon = document.getElementById('light-icon');
+const darkIcon = document.getElementById('dark-icon');
+const sections = document.querySelectorAll('.section-content');
+const modals = document.querySelectorAll('.modal');
+const modalLists = document.querySelectorAll('.modal ul');
+
+// Função para alternar entre o modo claro e escuro
+function toggleTheme() {
+    const isDarkMode = document.body.classList.contains('dark-mode');
+
+    if (isDarkMode) {
+        // Trocar para o modo claro
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        
+        sections.forEach(section => {
+            section.classList.remove('dark-mode');
+            section.classList.add('light-mode');
+        });
+
+        modals.forEach(modal => {
+            modal.classList.remove('dark-mode');
+            modal.classList.add('light-mode');
+        });
+
+        modalLists.forEach(list => {
+            list.classList.remove('dark-mode');
+            list.classList.add('light-mode');
+        });
+
+        lightIcon.style.display = 'inline';
+        darkIcon.style.display = 'none';
+    } else {
+        // Trocar para o modo escuro
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+
+        sections.forEach(section => {
+            section.classList.remove('light-mode');
+            section.classList.add('dark-mode');
+        });
+
+        modals.forEach(modal => {
+            modal.classList.remove('light-mode');
+            modal.classList.add('dark-mode');
+        });
+
+        modalLists.forEach(list => {
+            list.classList.remove('light-mode');
+            list.classList.add('dark-mode');
+        });
+
+        lightIcon.style.display = 'none';
+        darkIcon.style.display = 'inline';
+    }
+}
+
+// Evento para alternar tema no clique do botão
+toggleThemeBtn.addEventListener('click', toggleTheme);
+
+// Exibir estado inicial
+lightIcon.style.display = 'inline';
+darkIcon.style.display = 'none';
 
 window.addEventListener('DOMContentLoaded', event => {
 
